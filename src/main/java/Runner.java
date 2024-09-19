f6b99e3c71fd863dca02d3a513e27c473639fb5a
NurgulOzbek <gsgulnur74@gmail.com>
2024-09-19 20:58:39 +0300

    yeniden v2

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,10 +4,6 @@ public class Runner {
         System.out.println("version 1");
         System.out.println("version 2");
         System.out.println("version 2 devamı");
-        System.out.println("version 3");
-        System.out.println("version 4");
-
-
 
 
 
